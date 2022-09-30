43503706279da6320bbb4efb5fe9473e0155d457
Jakob Eschler <mail@jakob-eschler.de>
2022-09-30 16:36:38 +0200

    rfl starter data für 2022 aktualisiert

--- starter/rfl-weekly-starter-since-2020.R
+++ starter/rfl-weekly-starter-since-2020.R
@@ -2,9 +2,9 @@ library(ffscrapr)
 library(tidyverse)
 
 rfl <- ffscrapr::mfl_connect(league_id = "63018", season = 2022)
-starters <- ffscrapr::ff_starters(rfl, season = 2021) %>% 
+starters <- ffscrapr::ff_starters(rfl, season = 2022) %>% 
   select(season, week, franchise_id, starter_status, player_id, should_start, player_score, player_name, pos, team) %>% 
-  filter(week <= 13) %>% 
+  filter(week <= 3) %>% 
   distinct()
 
 utils::write.csv(starters, here("data/starter/rfl-starter-2022.csv"), row.names = F)
