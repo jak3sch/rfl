fc0adb3bdb00028026d2f715586b6f272dc15140
Jakob Eschler <mail@jakob-eschler.de>
2022-10-05 14:19:38 +0200

    update scrape starter code

--- scrape-data/starter/rfl-weekly-starter-since-2020.R
+++ scrape-data/starter/rfl-weekly-starter-since-2020.R
@@ -1,12 +1,13 @@
 library(ffscrapr)
 library(tidyverse)
+library(nflreadr)
 
 rfl <- ffscrapr::mfl_connect(league_id = "63018", season = 2022)
 starters <- ffscrapr::ff_starters(rfl, season = 2022) %>% 
   select(season, week, franchise_id, starter_status, player_id, should_start, player_score, player_name, pos, team) %>% 
-  filter(week <= 3) %>% 
+  filter(week <= nflreadr::get_current_week() - 1) %>% 
   distinct()
 
-utils::write.csv(starters, here("data/starter/rfl-starter-2022.csv"), row.names = F)
+utils::write.csv(starters, "data/starter/rfl-starter-2022.csv", row.names = F)
 
 rm(rfl, starters)
