4dd385fc286f8e0c2a58f8104d42d292c98fa8b8
Jakob Eschler <mail@jakob-eschler.de>
2023-04-13 16:46:01 +0200

    playerscores 2022 aktualisiert

--- scrape-data/rfl-scrape-playerscores.R
+++ scrape-data/rfl-scrape-playerscores.R
@@ -1,21 +1,20 @@
 library(tidyverse)
-library(ffscrapr)
 
-var.week <- 5
+var.week <- 17
 
-scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/2022/export?TYPE=playerScores&L=63018&APIKEY=&W=", var.week, "&YEAR=&PLAYERS=&POSITION=&STATUS=&RULES=&COUNT=&JSON=1")) %>%  
-  purrr::pluck("playerScores", "playerScore") %>% 
-  dplyr::tibble() %>% 
-  tidyr::unnest_wider(1) %>% 
+scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/2022/export?TYPE=playerScores&L=63018&APIKEY=&W=", var.week, "&YEAR=&PLAYERS=&POSITION=&STATUS=&RULES=&COUNT=&JSON=1")) %>%
+  purrr::pluck("playerScores", "playerScore") %>%
+  dplyr::tibble() %>%
+  tidyr::unnest_wider(1) %>%
   dplyr::left_join(
-    jsonlite::read_json("https://www55.myfantasyleague.com/2022/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1") %>% 
-      purrr::pluck("players", "player") %>% 
-      dplyr::tibble() %>% 
+    jsonlite::read_json("https://www55.myfantasyleague.com/2022/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1") %>%
+      purrr::pluck("players", "player") %>%
+      dplyr::tibble() %>%
       tidyr::unnest_wider(1),
     by = "id"
-  ) %>% 
-  dplyr::mutate(season = 2022) %>% 
-  dplyr::select(season, week, id, name, position, team, score) %>% 
+  ) %>%
+  dplyr::mutate(season = 2022) %>%
+  dplyr::select(season, week, id, name, position, team, score) %>%
   dplyr::rename(
     player_id = id,
     player_name = name,
@@ -23,9 +22,8 @@ scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/2022/exp
     points = score
   )
 
-loadData <- readRDS("data/playerscores/rfl-playerscores-2022.rds") %>% 
+loadData <- readRDS("data/playerscores/rfl-playerscores-2022.rds") %>%
   dplyr::filter(week < var.week)
 
 saveRDS(rbind(loadData, scores), "data/playerscores/rfl-playerscores-2022.rds")
-
 
