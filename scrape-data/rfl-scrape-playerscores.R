7070d1a9cb45b003228a7b2f94c4fbac92e2ba06
Jakob Eschler <mail@jakob-eschler.de>
2023-04-15 14:52:34 +0200

    player scores data code anpassungen

--- scrape-data/rfl-scrape-playerscores.R
+++ scrape-data/rfl-scrape-playerscores.R
@@ -1,19 +1,20 @@
 library(tidyverse)
 
-var.week <- 17
+var.season <- 2023
+var.week <- 1
 
-scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/2022/export?TYPE=playerScores&L=63018&APIKEY=&W=", var.week, "&YEAR=&PLAYERS=&POSITION=&STATUS=&RULES=&COUNT=&JSON=1")) %>%
+scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/", var.season, "/export?TYPE=playerScores&L=63018&APIKEY=&W=", var.week, "&YEAR=&PLAYERS=&POSITION=&STATUS=&RULES=&COUNT=&JSON=1")) %>%
   purrr::pluck("playerScores", "playerScore") %>%
   dplyr::tibble() %>%
   tidyr::unnest_wider(1) %>%
   dplyr::left_join(
-    jsonlite::read_json("https://www55.myfantasyleague.com/2022/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1") %>%
+    jsonlite::read_json(paste0("https://www55.myfantasyleague.com/", var.season, "/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1")) %>%
       purrr::pluck("players", "player") %>%
       dplyr::tibble() %>%
       tidyr::unnest_wider(1),
     by = "id"
   ) %>%
-  dplyr::mutate(season = 2022) %>%
+  dplyr::mutate(season = var.season) %>%
   dplyr::select(season, week, id, name, position, team, score) %>%
   dplyr::rename(
     player_id = id,
@@ -22,8 +23,9 @@ scores <- jsonlite::read_json(paste0("https://www55.myfantasyleague.com/2022/exp
     points = score
   )
 
-loadData <- readRDS("data/playerscores/rfl-playerscores-2022.rds") %>%
+loadData <- readRDS(paste0("data/playerscores/rfl-playerscores-", var.season, ".rds")) %>%
   dplyr::filter(as.numeric(week) < var.week)
 
-saveRDS(rbind(loadData, scores), "data/playerscores/rfl-playerscores-2022.rds")
+saveRDS(rbind(loadData, scores), paste0("data/playerscores/rfl-playerscores-", var.season, ".rds"))
 
+rm(var.week, var.season, scores, loadData)
