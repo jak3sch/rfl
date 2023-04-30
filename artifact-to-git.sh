2be941a04b135738385e23d35fdf63314d8e5225
Jakob Eschler <mail@jakob-eschler.de>
2023-05-01 00:00:02 +0200

    bugfix

--- artifact-to-git.sh
+++ artifact-to-git.sh
@@ -8,6 +8,9 @@ curl --user "$GITLAB_USER:$GITLAB_PASSWORD" \
      "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_COMMIT_REF_NAME/download?job=$CI_JOB_NAME" \
      --output output.csv
 
+# Aktuellen Stand des Remote-Repositories herunterladen
+git pull $REPO_URL main
+
 # Hinzufügen der CSV-Datei zum Git-Index
 git add output.csv
 
@@ -15,4 +18,4 @@ git add output.csv
 git commit -m "Automatisches Hinzufügen der CSV-Datei"
 
 # Pushen der Änderungen zum Remote-Repository
-git push $REPO_URL HEAD:$CI_COMMIT_REF_NAME
+git push $REPO_URL HEAD:main
