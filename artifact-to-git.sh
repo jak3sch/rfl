587e75a703768a8d557f673ba9573d4dbf752e31
Jakob Eschler <mail@jakob-eschler.de>
2023-04-30 23:47:45 +0200

    loginfos statt token

--- artifact-to-git.sh
+++ artifact-to-git.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# GitLab Personal Access Token
-TOKEN="glpat-EERdyyy6WGw1_JxDGef8"
-
 # Remote-Repository URL
 REPO_URL="https://gitlab.com/jakob-eschler/data-science/fantasy-football/rfl.git"
 
 # CSV-Datei herunterladen
-curl --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" \
+curl --user "$GITLAB_USER:$GITLAB_PASSWORD" \
      "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_COMMIT_REF_NAME/download?job=$CI_JOB_NAME" \
      --output output.csv
 
