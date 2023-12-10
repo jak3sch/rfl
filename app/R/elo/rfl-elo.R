6d842c6ea27f24d33d9c1345ab872c5240c6d093
Jakob Eschler <mail@jakob-eschler.de>
2023-12-10 11:29:00 +0100

    app elo bugfix

--- app/R/elo/rfl-elo.R
+++ app/R/elo/rfl-elo.R
@@ -30,39 +30,41 @@ elo_matchups_prev <- elo %>%
     "ELO Shift" = elo_shift
   )
 
-elo_matchups_next <- jsonlite::read_json(paste0(var.mflApiBase, "/export?TYPE=schedule&L=63018&APIKEY=&W=", nflreadr::get_current_week(), "&JSON=1")) %>%
-  purrr::pluck("schedule", "weeklySchedule", "matchup") %>%
-  tibble::tibble() %>%
-  tidyr::unnest_wider(1) %>%
-  tidyr::unnest_wider(1, names_sep = "_") %>%
-  tidyr::unnest_wider(1) %>%
-  dplyr::rename(franchise_id = id) %>%
-  dplyr::select(-spread, -isHome, -result) %>%
-  tidyr::unnest_wider(2) %>%
-  dplyr::rename(opponent_id = id) %>%
-  dplyr::select(franchise_id, opponent_id) %>%
-  dplyr::left_join(
-    elo %>%
-      dplyr::filter(season == max(season)) %>%
-      dplyr::filter(week == max(week)) %>%
-      dplyr::select(franchise_id, franchise_elo_pregame) %>% rename(franchise_elo = franchise_elo_pregame) %>%
-      dplyr::distinct(),
-    by = "franchise_id",
-    multiple = "all"
-  ) %>%
-  dplyr::left_join(
-    elo %>%
-      dplyr::filter(season == max(season)) %>%
-      dplyr::filter(week == max(week)) %>%
-      dplyr::select(franchise_id, franchise_elo_pregame) %>% rename(opponent_elo = franchise_elo_pregame) %>%
-      dplyr::distinct(),
-    by = c("opponent_id" = "franchise_id"),
-    multiple = "all",
-  ) %>%
-  dplyr::distinct() %>%
-  dplyr::mutate(win_pct = 1 / (10^(-(franchise_elo - opponent_elo) / 400 ) + 1)) %>%
-  dplyr::left_join(franchises %>% select(franchise_id, franchise_name), by = "franchise_id") %>%
-  dplyr::left_join(franchises %>% select(franchise_id, franchise_name) %>% rename(opponent_name = franchise_name), by = c("opponent_id" = "franchise_id")) %>%
-  dplyr::mutate("ELO Adv" = scales::percent(win_pct, 1)) %>%
-  dplyr::select(franchise_name, franchise_elo, "ELO Adv", opponent_elo, opponent_name) %>%
-  dplyr::rename(Home = franchise_name, "Home ELO" = franchise_elo, "Away ELO" = opponent_elo, Away = opponent_name)
+if (nflreadr::get_current_week() <= 13) {
+  elo_matchups_next <- jsonlite::read_json(paste0(var.mflApiBase, "/export?TYPE=schedule&L=63018&APIKEY=&W=", nflreadr::get_current_week(), "&JSON=1")) %>%
+    purrr::pluck("schedule", "weeklySchedule", "matchup") %>%
+    tibble::tibble() %>%
+    tidyr::unnest_wider(1) %>%
+    tidyr::unnest_wider(1, names_sep = "_") %>%
+    tidyr::unnest_wider(1) %>%
+    dplyr::rename(franchise_id = id) %>%
+    dplyr::select(-spread, -isHome, -result) %>%
+    tidyr::unnest_wider(2) %>%
+    dplyr::rename(opponent_id = id) %>%
+    dplyr::select(franchise_id, opponent_id) %>%
+    dplyr::left_join(
+      elo %>%
+        dplyr::filter(season == max(season)) %>%
+        dplyr::filter(week == max(week)) %>%
+        dplyr::select(franchise_id, franchise_elo_pregame) %>% rename(franchise_elo = franchise_elo_pregame) %>%
+        dplyr::distinct(),
+      by = "franchise_id",
+      multiple = "all"
+    ) %>%
+    dplyr::left_join(
+      elo %>%
+        dplyr::filter(season == max(season)) %>%
+        dplyr::filter(week == max(week)) %>%
+        dplyr::select(franchise_id, franchise_elo_pregame) %>% rename(opponent_elo = franchise_elo_pregame) %>%
+        dplyr::distinct(),
+      by = c("opponent_id" = "franchise_id"),
+      multiple = "all",
+    ) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(win_pct = 1 / (10^(-(franchise_elo - opponent_elo) / 400 ) + 1)) %>%
+    dplyr::left_join(franchises %>% select(franchise_id, franchise_name), by = "franchise_id") %>%
+    dplyr::left_join(franchises %>% select(franchise_id, franchise_name) %>% rename(opponent_name = franchise_name), by = c("opponent_id" = "franchise_id")) %>%
+    dplyr::mutate("ELO Adv" = scales::percent(win_pct, 1)) %>%
+    dplyr::select(franchise_name, franchise_elo, "ELO Adv", opponent_elo, opponent_name) %>%
+    dplyr::rename(Home = franchise_name, "Home ELO" = franchise_elo, "Away ELO" = opponent_elo, Away = opponent_name)
+}
