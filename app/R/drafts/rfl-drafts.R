173edbb3d727fbcd1d9771635afb4402f4fc981c
Jakob Eschler <mail@jakob-eschler.de>
2024-04-29 18:58:12 +0200

    app update

--- app/R/drafts/rfl-drafts.R
+++ app/R/drafts/rfl-drafts.R
@@ -1,5 +1,8 @@
+rfl_drafts_data <- readr::read_csv("https://raw.githubusercontent.com/jak3sch/rfl/main/data/drafts/rfl-draft.csv", col_types = "iicccccccci")
+
+
 rfl_drafts <- reactive({
-  rfl_drafts <- readr::read_csv("https://raw.githubusercontent.com/jak3sch/rfl/main/data/drafts/rfl-draft.csv", col_types = "iicccccccci") %>%
+  rfl_drafts <- rfl_drafts_data %>%
     dplyr::filter(
       !is.na(position),
       overall >= input$selectPicks[1] & overall <= input$selectPicks[2]
@@ -18,4 +21,25 @@ rfl_drafts <- reactive({
     dplyr::arrange(first_pick) %>%
     dplyr::mutate(pos_rank = dplyr::row_number()) %>%
     dplyr::ungroup()
+})
+
+nfl_draft_rounds <- reactive({
+  nfl_draft_rounds <- rfl_drafts_data %>%
+    dplyr::filter(
+      !is.na(gsis_id),
+      overall >= input$selectPicks[1] & overall <= input$selectPicks[2]
+    ) %>%
+    dplyr::left_join(
+      nflreadr::load_draft_picks() %>% 
+        dplyr::filter(!is.na(gsis_id), season >= 2016) %>% 
+        dplyr::select(gsis_id, season, round) %>% 
+        dplyr::rename(round_nfl = round),
+      by = c("gsis_id", "season"),
+      relationship = "many-to-many"
+    ) %>%
+    dplyr::mutate(
+      round_nfl = ifelse(is.na(gsis_id), 8, round_nfl)
+    ) %>% 
+    dplyr::group_by(position, round, round_nfl) %>% 
+    dplyr::summarise(count = n(), .groups = "drop")
 })
