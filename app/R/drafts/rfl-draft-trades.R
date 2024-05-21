1aa972209fde582c75306273800dcd12a65914b9
Jakob Eschler <mail@jakob-eschler.de>
2024-05-22 01:33:20 +0200

    rfl draft trade code update

--- app/R/drafts/rfl-draft-trades.R
+++ app/R/drafts/rfl-draft-trades.R
@@ -17,46 +17,53 @@ rfl_draft_trades <- trades %>%
       asset_ids = paste(asset_id, collapse = ","),
       franchise_ids = paste(franchise_id, collapse = ",")
     ) %>%
-  dplyr::filter(franchise_id == "0007")
-
-rfl_draft_trades_sends <- rfl_draft_trades %>%
-  dplyr::filter(trade_side == "franchise_1") %>%
-  dplyr::group_by(franchise_id) %>%
+  dplyr::group_by(trade_id, trade_side, asset_types) %>%
   dplyr::summarise(
     date = dplyr::first(date),
     asset_ids = dplyr::first(asset_ids),
     asset_types = dplyr::first(asset_types),
     franchise_ids = dplyr::first(franchise_ids),
-    asset_names = paste(asset_name, collapse = "\n"),
+    asset_names = paste(asset_name, collapse = "--"),
     franchise_name = dplyr::first(franchise_name),
     .groups = "drop"
   ) %>%
+  dplyr::select(trade_id, date, trade_side, asset_names, franchise_name) %>%
+  tidyr::spread(trade_side, asset_names) %>%
+  dplyr::group_by(trade_id) %>%
+  dplyr::arrange(franchise_1) %>%
   dplyr::mutate(
-    asset_names = paste0(franchise_name, " sends\n", asset_names)
-  )
-  dplyr::select(trade_id, date, trade_side, asset_names) %>%
-  tidyr::spread(trade_side, asset_names)
+    franchise_gets = ifelse(is.na(franchise_2), lead(franchise_2), lag(franchise_1)),
+    franchise_sends = ifelse(is.na(franchise_1), franchise_2, franchise_1),
+  ) %>%
+  dplyr::select(-franchise_1, -franchise_2) %>%
+  dplyr::ungroup()
+  #filter(franchise_name == "Jena Dragons")
 
-rfl_draft_trades_gets <- rfl_draft_trades %>%
-  dplyr::filter(trade_side == "franchise_2") %>%
-  dplyr::group_by(franchise_id) %>%
+draft_trades_get <- rfl_draft_trades %>%
+  dplyr::select(franchise_name, franchise_gets) %>%
+  tidyr::separate_rows(franchise_gets, sep = "--") %>%
+  dplyr::group_by(franchise_name) %>%
+  dplyr::arrange(franchise_gets) %>%
   dplyr::summarise(
-    date = dplyr::first(date),
-    asset_ids = dplyr::first(asset_ids),
-    asset_types = dplyr::first(asset_types),
-    franchise_ids = dplyr::first(franchise_ids),
-    asset_get = paste(asset_name, collapse = "\n"),
-    franchise_name = dplyr::first(franchise_name),
+    franchise_gets = paste(franchise_gets, collapse = "\n"),
     .groups = "drop"
   ) %>%
   dplyr::mutate(
-    asset_get = paste0(franchise_name, " gets\n", asset_get)
+    franchise_gets = paste0(franchise_name, " gets\n", franchise_gets),
   )
 
-rfl_draft_trades_combined <- rfl_draft_trades_sends %>%
-  dplyr::select(franchise_id, franchise_name, asset_send) %>%
-  dplyr::left_join(
-    rfl_draft_trades_gets %>%
-      dplyr::select(franchise_id, asset_get),
-    by = c("franchise_id")
+draft_trades_send <- rfl_draft_trades %>%
+  dplyr::select(franchise_name, franchise_sends) %>%
+  tidyr::separate_rows(franchise_sends, sep = "--") %>%
+  dplyr::group_by(franchise_name) %>%
+  dplyr::arrange(franchise_sends) %>%
+  dplyr::summarise(
+    franchise_sends = paste(franchise_sends, collapse = "\n"),
+    .groups = "drop"
+  ) %>%
+  dplyr::mutate(
+    franchise_sends = paste0(franchise_name, " sends\n", franchise_sends),
   )
+
+draft_trades_combined <- draft_trades_get %>%
+  dplyr::left_join(draft_trades_send, by = "franchise_name")
